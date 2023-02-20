6d64467a6a1a01c87b02a4fdbd25a5bd68190ecb
Kostyantyn Gromov <kostyantyn_gromov@comcast.com>
2023-02-21 01:16:24 +0200

    Add profiles for cloud stream

--- src/main/java/com/domestic/weather/weatherarchive/WeatherArchiveApplication.java
+++ src/main/java/com/domestic/weather/weatherarchive/WeatherArchiveApplication.java
@@ -5,11 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -21,18 +17,9 @@ public class WeatherArchiveApplication {
     }
 
     @Bean
-    @ConditionalOnProperty(value = "weather.populate", havingValue = "true")
     ApplicationRunner applicationRunner(SyncService syncService) {
         return args -> {
            syncService.syncDailyTemperature();
         };
-    }
-
-    private static class PopulateTemperatureCondition implements Condition {
-
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return Boolean.parseBoolean(context.getEnvironment().getProperty("weather.populate", "true"));
-        }
     }
 }
